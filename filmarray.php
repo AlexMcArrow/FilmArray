e4962c028a7e1433c0e1f303e41396383520f3c8
Alex McArrow <alex.mcarrow@gmail.com>
2012-04-27 23:40:53 +0500

    bitborn FASv0.1 FAFXv0.2

--- filmarray.php
+++ filmarray.php
@@ -5,8 +5,15 @@
  * @copyright AlexMcArrow 2008-2012
  * @link http://mcarrow.ru/
  *
+ * =FilmArrayStudio=
+ * =FilmArrayFX=
  */
+new FilmArrayStudio();
+new FilmArrayFX();
+
 class FilmArrayStudio {
+
+    const FAS_version = '0.1';
 
     public static $Scenario;
     public static $Movie;
@@ -30,11 +37,10 @@ class FilmArrayStudio {
         return FALSE;
     }
 
-    public static function NewFilm ($name, $author, $width = 16) {
+    public static function NewFilm ($name, $author) {
         self::$Scenario = array (
             'name' => $name,
             'author' => $author,
-            'width' => $width,
             'frames' => array ()
         );
         self::$Movie = array ();
@@ -50,44 +56,138 @@ class FilmArrayStudio {
     }
 
     public static function MakeMovie () {
-        echo 'Making';
+        self::$Movie = array ();
+        foreach (self::$Scenario['frames'] as $key => $value) {
+            self::$Movie = array_merge (self::$Movie, FilmArrayFX::DrawFrames ($value));
+        }
+    }
+
+    public static function SaveMovie ($filename) {
+        self::MakeMovie ();
+        $BODY = '<pre style="font-size:2em;">';
+        $BODY .= print_r (self::$Movie, TRUE);
+        $BODY .= '<hr/>FilmArrayStudio ' . self::FAS_version . '<br/>FilmArrayFX ' . FilmArrayFX::FAFX_version . '<hr/>';
+        file_put_contents ($filename . '.html', $BODY);
     }
 
 }
 
 class FilmArrayFX {
 
-    public static function Titles ($titles) {
-        if (is_array ($titles)) {
-            foreach ($titles as $text) {
-                self::_set_title ($text);
-            }
-        } else {
-            self::_set_title ($titles);
-        }
+    const FAFX_version = '0.3';
+
+    private static $Width;
+    private static $Height;
+    private static $FPS;
+    private static $NullBit;
+    private static $Templates;
+
+    function __construct () {
+        self::$Width = 32;
+        self::$Height = 10;
+        self::$FPS = 24;
+        self::$NullBit = ' ';
+        self::$Templates = array ();
+        self::_gen_null_screen ();
     }
 
-    private static function _set_title ($text) {
-        $FrameWidth = FilmArrayStudio::_get ('width');
-        $FrameHeight = round ($FrameWidth / 4 * 3);
-        $TextWidth = mb_strlen ($text);
-        $left = round (($FrameWidth / 2) - ($TextWidth / 2));
-        $top = round ($FrameHeight / 2);
+    public static function Titles ($titles, $speed) {
+        /// scrolling titles
+    }
+
+    public static function Title ($titles, $fillbit = FALSE, $length = 2) {
+        self::_set_title ($titles, $fillbit, $length);
+    }
+
+    public static function NullScreen ($frames) {
         $Frames = FilmArrayStudio::_get ('frames');
         $Frames[] = array (
-            'type' => 'centext',
+            'type' => 'null',
             'data' => array (
-                'length' => '4',
-                'top' => $top,
-                'left' => $left,
-                'text' => $text
+                'length' => $frames
             )
         );
         FilmArrayStudio::_set ('frames', $Frames);
     }
 
-}
+    public static function FillScreen ($bit, $frames) {
+        $Frames = FilmArrayStudio::_get ('frames');
+        $Frames[] = array (
+            'type' => 'fill',
+            'data' => array (
+                'length' => $frames,
+                'bit' => $bit
+            )
+        );
+        FilmArrayStudio::_set ('frames', $Frames);
+    }
 
-class FilmArrayProjector {
+    public static function DrawFrames ($rules) {
+        $OUT = array ();
+        switch ($rules['type']) {
+            case 'null':
+                for ($len = 0; $len < ($rules['data']['length'] * self::$FPS); $len++) {
+                    $OUT[] = self::$Templates['null'];
+                }
+                break;
+            case 'fill':
+                for ($len = 0; $len < ($rules['data']['length'] * self::$FPS); $len++) {
+                    $OUT[] = self::_gen_fill_screen ($rules['data']['bit']);
+                }
+                break;
+            case 'centext':
+                if ($rules['data']['bit'] !== FALSE) {
+                    $frame = self::_gen_fill_screen ($rules['data']['bit']);
+                } else {
+                    $frame = self::$Templates['null'];
+                }
+                for ($index = 0; $index < mb_strlen ($rules['data']['text'], 'UTF-8'); $index++) {
+                    $frame[$rules['data']['top']][$rules['data']['left'] + $index] = $rules['data']['text'][$index];
+                }
+                for ($len = 0; $len < ($rules['data']['length'] * self::$FPS); $len++) {
+                    $OUT[] = $frame;
+                }
+                break;
+
+            default:
+                $OUT[] = self::$Templates['null'];
+                break;
+        }
+        return $OUT;
+    }
+
+    private static function _gen_null_screen () {
+        self::$Templates['null'] = self::_gen_fill_screen (self::$NullBit);
+    }
+
+    private static function _gen_fill_screen ($bit) {
+        $FILL = array ();
+        $line = '';
+        for ($y = 0; $y <= self::$Width; $y++) {
+            $line .= $bit;
+        }
+        for ($x = 0; $x < self::$Height; $x++) {
+            $FILL[] = $line;
+        }
+        return $FILL;
+    }
+
+    private static function _set_title ($text, $bit, $length) {
+        $TextWidth = mb_strlen ($text, 'UTF-8');
+        $left = round ((self::$Width / 2) - ($TextWidth / 2));
+        $top = round (self::$Height / 2);
+        $Frames = FilmArrayStudio::_get ('frames');
+        $Frames[] = array (
+            'type' => 'centext',
+            'data' => array (
+                'length' => $length,
+                'top' => $top,
+                'left' => $left,
+                'text' => $text,
+                'bit' => $bit
+            )
+        );
+        FilmArrayStudio::_set ('frames', $Frames);
+    }
 
 }
--- index.php
+++ index.php
@@ -6,16 +6,27 @@
  * @link http://mcarrow.ru/
  *
  */
+header ("Content-Type: text/html; charset=utf-8");
 include 'filmarray.php';
 
-new FilmArrayStudio();
+FilmArrayStudio::NewFilm ('Born of BIT', 'AlexMcArrow');
 
-FilmArrayStudio::NewFilm ('Test', 'AlexMcArrow', 16);
+FilmArrayFX::NullScreen (5);
+FilmArrayFX::Title ('Born of BIT');
+FilmArrayFX::FillScreen ('0', 5);
+FilmArrayFX::Title ('1', '0', 3);
+FilmArrayFX::Title ('111', '0', 1);
+FilmArrayFX::Title ('11111', '0', 1);
+FilmArrayFX::Title ('1111111', '0', 1);
+FilmArrayFX::FillScreen ('1', 2);
+FilmArrayFX::Title ('The End');
+FilmArrayFX::Title ('Alex McArrow', FALSE, 1);
+FilmArrayFX::Title ('NTMan', FALSE, 1);
+FilmArrayFX::Title ('Denis', FALSE, 1);
+FilmArrayFX::Title ('2012');
 
-FilmArrayFX::Titles ('The End');
+FilmArrayStudio::MakeMovie ();
 
-//FilmArrayStudio::SaveProject ('test');
+FilmArrayStudio::SaveProject ('bitborn');
 
-echo '<pre>';
-print_r (FilmArrayStudio::$Scenario);
-print_r (FilmArrayStudio::$Movie);
+FilmArrayStudio::SaveMovie ('bitborn');
